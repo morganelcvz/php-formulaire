2685fcdf4df872d669e3c489fc3682b81b324d80
‘morganelcvz’ <morgane.lecalvez@outlook.fr>
2025-03-04 11:02:23 +0100

    ajout du forumulaire terminé

--- inscription.php
+++ inscription.php
@@ -4,7 +4,7 @@
 // var_dump($_GET); 
 
 // super globale $_POST 
-var_dump($_POST);
+// var_dump($_POST);
 
 //pas de chiffres
 //pas de vide
@@ -16,100 +16,98 @@ $errors = [];
 $regex_name = "/^[a-zA-Z]+$/";
 
 // NOM 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    var_dump($_POST);
-    if(isset($_POST['nom'])){
-        if(empty($_POST['nom'])){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['nom'])) {
+        if (empty($_POST['nom'])) {
             $errors['nom'] = 'ce champ est obligatoire';
-        } elseif (!preg_match($regex_name, $_POST['nom'])){
+        } elseif (!preg_match($regex_name, $_POST['nom'])) {
             $errors['nom'] = 'caractère non autorisé';
         }
     }
-}
+
 
 // PRENOM
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    var_dump($_POST);
-    if(isset($_POST['prénom'])){
-        if(empty($_POST['prénom'])){
+
+    if (isset($_POST['prénom'])) {
+        if (empty($_POST['prénom'])) {
             $errors['prénom'] = 'ce champ est obligatoire';
-        } elseif (!preg_match($regex_name, $_POST['prénom'])){
+        } elseif (!preg_match($regex_name, $_POST['prénom'])) {
             $errors['prénom'] = 'caractère non autorisé';
         }
     }
-}
+
 
 // E-MAIL
 $regex_mail = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
 
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    var_dump($_POST);
-    if(isset($_POST['email'])){
-        if(empty($_POST['email'])){
+    if (isset($_POST['email'])) {
+        if (empty($_POST['email'])) {
             $errors['email'] = 'ce champ est obligatoire';
-        } elseif (!preg_match($regex_mail, $_POST['email'])){
+        } elseif (!preg_match($regex_mail, $_POST['email'])) {
             $errors['email'] = 'e-mail non valide';
         }
     }
-}
+
 
 // MOT DE PASSE 
-$regex_password = "/^\S+$/"; 
+$regex_password = "/^\S+$/";
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    var_dump($_POST);
-    if(isset($_POST['password'])){
-        if(empty($_POST['password'])){
+    if (isset($_POST['password'])) {
+        if (empty($_POST['password'])) {
             $errors['password'] = 'ce champ est obligatoire';
-        } elseif (!preg_match($regex_password, $_POST['password'])){
+        } elseif (!preg_match($regex_password, $_POST['password'])) {
             $errors['password'] = 'caractère non autorisé';
         }
     }
-}
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    var_dump($_POST);
-    if(isset($_POST['password-confirm'])){
-        if(empty($_POST['password-confirm'])){
+
+    if (isset($_POST['password-confirm'])) {
+        if (empty($_POST['password-confirm'])) {
             $errors['password-confirm'] = 'ce champ est obligatoire';
-        } elseif (!preg_match($regex_password, $_POST['password-confirm'])){
+        } elseif (!preg_match($regex_password, $_POST['password-confirm'])) {
             $errors['password-confirm'] = 'caractère non autorisé';
         } elseif ($_POST['password-confirm'] != $_POST['password']) {
             $errors['password-confirm'] = 'le mot de passe ne correspond pas';
         }
     }
-}
+
 
 // DATE DE NAISSANCE 
-$regex_date = "/^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$/";
+$regex_date = "/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/";
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    var_dump($_POST);
-    if(isset($_POST['dob'])){
-        if(empty($_POST['dob'])){
+
+    if (isset($_POST['dob'])) {
+        if (empty($_POST['dob'])) {
             $errors['dob'] = 'ce champ est obligatoire';
-        } elseif (!preg_match($regex_date, $_POST['dob'])){
+        } elseif (!preg_match($regex_date, $_POST['dob'])) {
             $errors['dob'] = 'date non valide';
+        } elseif ($_POST['dob'] > "2024-12-12") {
+            $errors['dob'] = "année non valide";
         }
     }
-}
 
 // GENRE 
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    var_dump($_POST);
-    if(isset($_POST['genre'])){
-        if(empty($_POST['genre'])){
-            $errors['genre'] = 'ce champ est obligatoire';
-        } elseif (!preg_match($regex_genre, $_POST['genre'])){
-            $errors['genre'] = 'date non valide';
-        }
+$genre = ["homme", "femme", "autre"];
+
+    if (!isset($_POST['genre'])) {
+        $errors['genre'] = 'ce champ est obligatoire';
+    } elseif (!in_array($_POST['genre'], $genre)) {
+        $errors['genre'] = 'genre inconnu';
     }
+
+
+// CONDITIONS D'UTILISATIONS 
+
+
+    var_dump($_POST);
+    if (!isset($_POST['conditions'])) {
+        $errors['conditions'] = "Veuillez accepter les conditions d'utilisations";
+    }
+    
 }
 
-
-
+var_dump($_POST);
 var_dump($errors);
 
 // //si mon tableau d'erreur est vide redirection
@@ -118,7 +116,7 @@ var_dump($errors);
 //     exit;
 // }
 
-?> 
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -133,7 +131,7 @@ var_dump($errors);
 <body>
     <div class="main">
         <h2>Formulaire d'inscription</h2>
-        <form method="post">
+        <form method="post" novalidate>
             <label for="nom">Nom</label>
             <span class="required"><?= $errors['nom'] ?? '' ?></span>
             <input type="text" id="nom" name="nom" value="<?= $_POST['nom'] ?? '' ?>">
@@ -148,11 +146,11 @@ var_dump($errors);
             <br>
             <label for="password">Mot de passe</label>
             <span class="mail-error"><?= $errors['password'] ?? '' ?></span>
-            <input type="password" id="password" name="password">
+            <input type="password" id="password" name="password" value="<?= $_POST['password'] ?? '' ?>">
             <br>
             <label for="password-confirm">Confirmation du mot de passe</label>
             <span class="mdp-error"><?= $errors['password-confirm'] ?? '' ?></span>
-            <input type="password" id="password-confirm" name="password-confirm">
+            <input type="password" id="password-confirm" name="password-confirm" value="<?= $_POST['password-confirm'] ?? '' ?>">
             <br>
             <label for="dob">Date de naissance</label>
             <span class="required"><?= $errors['dob'] ?? '' ?></span>
@@ -161,18 +159,21 @@ var_dump($errors);
             <label for="genre">Genre</label>
             <span class="required"><?= $errors['genre'] ?? '' ?></span>
             <select name="genre" id="genre">
-                <option value="vide"></option>
-                <option value="homme">Homme</option>
-                <option value="femme">Femme</option>
-                <option value="autre">Autre</option>
+                <option value="" selected disabled></option>
+                <option value="homme" <?= isset($_POST['genre']) && $_POST['genre'] == 'homme' ? 'selected' : '' ?>>Homme</option>
+                <option value="femme" <?= isset($_POST['genre']) && $_POST['genre'] == 'femme' ? 'selected' : '' ?>>Femme</option>
+                <option value="autre" <?= isset($_POST['genre']) && $_POST['genre'] == 'autre' ? 'selected' : '' ?>>Autre</option>
             </select>
             <br>
             <div class="check">
-                <input type="checkbox" id="conditions" name="conditions" value="conditions">
+                <input type="checkbox" id="conditions" name="conditions" value="1">
                 <label for="conditions">accepter les conditions d'utilisation</label>
-                <br/>
-                <br/>
-                <span class="accept">Veuillez accepter les conditions d'utilisation</span>
+                <br />
+                <br />
+                <?php if (isset($errors['conditions'])) { ?>
+                    <span class="accept"><?= $errors['conditions']?></span>
+                <?php } ?>
+
             </div>
             <div class="send">
                 <button type="submit">envoyer</button>
